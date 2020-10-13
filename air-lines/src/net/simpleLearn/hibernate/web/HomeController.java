c051ba494e369c7f8475bff38f5641cdaaaa9c16
Hazem Musallam <devhazemmsallam@gmail.com>
2020-10-13 13:58:54 +0300

    Fix Controller

--- air-lines/src/net/simpleLearn/hibernate/web/HomeController.java
+++ air-lines/src/net/simpleLearn/hibernate/web/HomeController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
  * @author hazem.musallamau
  */
 
-@WebServlet({"/home","/"})
+@WebServlet({ "/home" })
 public class HomeController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
